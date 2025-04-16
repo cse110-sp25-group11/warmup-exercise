8910ab4c069b6d0b7560f81d4c34423e7728aca9
BenMiller0 <bem002@ucsd.edu>
2025-04-16 16:39:49 -0700

    added member values to constructure in card

--- js_team/part1/script.js
+++ js_team/part1/script.js
@@ -1,25 +1,29 @@
 
 
 class Deck {
+    
     constructor(){
-
+        
     }
 
 
     // Pick a random card that has not been choosen 
     pickCard(){
+        getRandomInt(3); // pick random number 0-4 for suit
 
 
     }
 }
 
 class Card {
+
     constructor(value,suit){
-        
+        this.value = value;
+        this.suit = suit;
     }
 
     flip(){
-        
+
     }
 }
 
