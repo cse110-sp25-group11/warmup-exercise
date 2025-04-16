ed5a7c635500eb4bb3c1faa4a040f1e6b5624fb2
BenMiller0 <bem002@ucsd.edu>
2025-04-16 16:50:06 -0700

    backtracked from random number idea

--- js_team/part1/script.js
+++ js_team/part1/script.js
@@ -3,16 +3,16 @@
 class Deck {
     
     constructor(){
-        this.cardsdrawn = []; // Declare empty array of cards that have not been drawn
+        
     }
 
 
     // Pick a random card that has not been choosen 
     pickCard(){
-        getRandomInt(3); // pick random number 0-3 for suit
-        getRandomInt(12); // pick random number 0-12 for number
+
 
     }
+
 }
 
 class Card {
