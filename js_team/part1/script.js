39ee7ebce8f99a9d914a8abc9d639a6272e62e04
BenMiller0 <bem002@ucsd.edu>
2025-04-16 16:41:22 -0700

    added cards drawn arraw to keep track of drawn cards

--- js_team/part1/script.js
+++ js_team/part1/script.js
@@ -3,14 +3,14 @@
 class Deck {
     
     constructor(){
-        
+        this.cardsdrawn = []; // Declare empty array of cards that have not been drawn
     }
 
 
     // Pick a random card that has not been choosen 
     pickCard(){
-        getRandomInt(3); // pick random number 0-4 for suit
-
+        getRandomInt(3); // pick random number 0-3 for suit
+        getRandomInt(12); // pick random number 0-12 for number
 
     }
 }
